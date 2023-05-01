636446072409a536df4f16e7fc8504803680001b
Gopal Walia <k.gopal.2002@icloud.com>
2023-05-01 19:24:28 -0400

    error popped up, now DONE!!!!!!!

---
+++ app/src/main/java/edu/temple/superbrowser/BookmarkListActivity.kt
@@ -0,0 +1,62 @@
+package edu.temple.superbrowser
+
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import android.widget.ImageButton
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+class BookmarkListActivity : AppCompatActivity() {
+
+    private lateinit var bookmarkManager: BookmarkManager
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_bookmark)
+
+        setTitle("Bookmarks")
+
+        bookmarkManager = BookmarkManager(this)
+
+        val bookmarks = bookmarkManager.getAllBookmarks()
+
+        findViewById<ImageButton>(R.id.exitBookmarkButton).setOnClickListener {
+            finish()
+        }
+
+
+        val recyclerView = findViewById<RecyclerView>(R.id.bookmark_recycler_view)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.adapter = BookmarkListAdapter(bookmarks,
+            object : BookmarkListAdapter.OnBookmarkClickListener {
+                override fun onBookmarkClick(url: String) {
+                    val resultIntent = Intent()
+                    resultIntent.putExtra("selectedURL", url)
+                    setResult(Activity.RESULT_OK, resultIntent)
+                    finish()
+                }
+
+
+            },
+            object : BookmarkListAdapter.OnDeleteClickListener {
+                override fun onDeleteClick(url: String) {
+                    deleteConfirmationDialog(url)
+                }
+            })
+    }
+
+    private fun deleteConfirmationDialog(url: String) {
+        AlertDialog.Builder(this)
+            .setTitle(R.string.delete_bookmark_title)
+            .setMessage(R.string.delete_bookmark_message)
+            .setPositiveButton(R.string.delete) { _, _ ->
+                bookmarkManager.deleteBookmark(url)
+                recreate()
+            }
+            .setNegativeButton(R.string.cancel, null)
+            .show()
+    }
+}
